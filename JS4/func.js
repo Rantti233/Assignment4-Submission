62accae01adb69aec7e7027c220bbf7ec66a57c3
Rantti233 <98371463+Rantti233@users.noreply.github.com>
2022-01-30 16:40:07 +0530

    Assignment 4 submitted

---
+++ JS4/func.js
@@ -0,0 +1,8 @@
+function table(){
+    //const n= prompt("Enter a positive integer:")
+    for(let i=1;i<=10;i++)
+    console.log(n+"*"+i+"="+n*i)
+    
+}
+const n = prompt("Enter a positive integer :")
+table(n)
